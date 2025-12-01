83f8946cabfdd607c582cceb2112564c2b690d86
lucienb3 <lucienb3@nycstudents.net>
2025-12-01 10:10:44 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,1 +1,16 @@
-print('hello world')
+from numpy import random
+
+rndstory = random.randint(1,3)
+print(rndstory)
+
+
+
+if rndstory == 1:
+	adjective1 = input("write a adjective: ")
+	noun1 = input('write a noun: ')
+	action1 = input('write a present tense action: ')
+	action2 = input('write a past tense action: ')
+	print(f"once opon a time there was a {adjective1} {noun1} who liked to {action1}. \n The {adjective1} {noun1} liked to {action1} so much that the {adjective1} {noun1} {action2}. ")
+
+
+print(f"")
