b0634e4a2a2ac0fe62f60beb1ed5f27a572f1147
lucienb3 <lucienb3@nycstudents.net>
2025-12-01 10:55:34 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -13,4 +13,13 @@ if rndstory == 1:
 	print(f"once opon a time there was a {adjective1} {noun1} who liked to {action1}. \n The {adjective1} {noun1} liked to {action1} so much that the {adjective1} {noun1} {action2}. ")
 
 
-print(f"")
+
+
+if rndstory == 2:
+	adjective1 = input("write a adjective: ")
+	noun1 = input("write a noun: ")
+	adjective2 = input("write a adjective: ")
+	animal1 = input("write an animal: ")
+	verb1 = input("write a verb: ")
+	emotion1 = input("write a emotion1: ")
+	print(f"Today, I decided to visit the park to enjoy the beautiful {adjective1} weather. I packed my {noun1} and headed out. On the way, I saw a {adjective2} {animal1} {verb1} down the street. It looked very {emotion1}")
